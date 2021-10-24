4ba0e4cbd7482ae0f587cd494af93988276d3070
Ana Blanco <anablancodelgado@gmail.com>
2021-10-24 23:19:06 +0200

    Update app_cluster.py

--- app_cluster.py
+++ app_cluster.py
@@ -13,14 +13,14 @@ def home():
 
 @app.route('/cluster', methods=['GET'])
 def api_cluster():
-    new_user = pd.DataFrame.from_dict(request.get_json()['interests'], orient='index').T
+    new_user = pd.DataFrame.from_dict(request.get_json(force=True)['interests'], orient='index').T
     new_user = new_user.reindex(columns = ['climbing', 'fitness', 'football', 'paddle', 'running', 'surf',
                                         'trecking', 'volley', 'bookstores', 'concerts', 'guidedVisits',
                                         'movies', 'museums', 'parks', 'ruralTourism', 'theaters', 'burguers',
                                         'chinese', 'indian', 'italian', 'mediterranean', 'sushi', 'coffee',
                                         'drinks', 'parties', 'sunsets'])
     new_user = new_user.astype('int')
-    _id = request.get_json()['_id']
+    _id = request.get_json(force=True)['_id']
 
     cluster = model.predict(new_user)
     cluster = int(cluster[0])
