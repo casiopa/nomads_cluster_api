ebb2866f960b56981eb5ddef48cded5e9a191251
Ana Blanco <anablancodelgado@gmail.com>
2021-10-24 23:16:43 +0200

    Corrected Read json Full Stack

--- app_cluster.py
+++ app_cluster.py
@@ -13,12 +13,20 @@ def home():
 
 @app.route('/cluster', methods=['GET'])
 def api_cluster():
-
+    new_user = pd.DataFrame.from_dict(request.get_json()['interests'], orient='index').T
+    new_user = new_user.reindex(columns = ['climbing', 'fitness', 'football', 'paddle', 'running', 'surf',
+                                        'trecking', 'volley', 'bookstores', 'concerts', 'guidedVisits',
+                                        'movies', 'museums', 'parks', 'ruralTourism', 'theaters', 'burguers',
+                                        'chinese', 'indian', 'italian', 'mediterranean', 'sushi', 'coffee',
+                                        'drinks', 'parties', 'sunsets'])
+    new_user = new_user.astype('int')
     _id = request.get_json()['_id']
 
+    cluster = model.predict(new_user)
+    cluster = int(cluster[0])
+    print(cluster)
 
-
-    return jsonify({"_id": _id})
+    return jsonify({"_id": _id, "cluster": cluster})
 
 if __name__ == '__main__':
     model = load('model/model.pkl')
