03303b1240af0f3cb402fc15195b04eed458f799
Ana Blanco <anablancodelgado@gmail.com>
2021-10-24 21:45:07 +0200

    Read Full Stack JSON

--- app_cluster.py
+++ app_cluster.py
@@ -1,12 +1,9 @@
 from flask import Flask, request, jsonify
 from joblib import load
 import pandas as pd
-from distutils.util import strtobool
-from sklearn.cluster import KMeans
 
 
 app = Flask(__name__)
-# app.config["DEBUG"] = True
 
 
 @app.route('/', methods=['GET'])
@@ -16,16 +13,20 @@ def home():
 
 @app.route('/cluster', methods=['GET'])
 def api_cluster():
-    new_user = pd.DataFrame.from_dict(request.get_json(), orient='index').T
-    user_id = new_user['user_id'].values[0]
-    new_user.drop('user_id', axis='columns', inplace=True)
-    new_user = new_user.applymap(strtobool)
+    new_user = pd.DataFrame.from_dict(request.get_json()['interests'], orient='index').T
+    new_user = new_user.reindex(columns = ['climbing', 'fitness', 'football', 'paddle', 'running', 'surf',
+                                        'trecking', 'volley', 'bookstores', 'concerts', 'guidedVisits',
+                                        'movies', 'museums', 'parks', 'ruralTourism', 'theaters', 'burguers',
+                                        'chinese', 'indian', 'italian', 'mediterranean', 'sushi', 'coffee',
+                                        'drinks', 'parties', 'sunsets'])
+    new_user = new_user.astype('int')
+    _id = request.get_json()['_id']
 
     cluster = model.predict(new_user)
     cluster = int(cluster[0])
     print(cluster)
 
-    return jsonify({'user_id': user_id, 'cluster': cluster})
+    return jsonify({"_id": _id, "cluster": cluster})
 
 if __name__ == '__main__':
     model = load('model/model.pkl')
