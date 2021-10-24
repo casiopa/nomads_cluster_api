644d29bb7fe94f5f54e2bdf87832e68f598f2377
Ana Blanco <anablancodelgado@gmail.com>
2021-10-24 16:16:23 +0200

    Read json, model baseline

--- app_cluster.py
+++ app_cluster.py
@@ -1,18 +1,32 @@
 from flask import Flask, request, jsonify
-import random
+from joblib import load
+import pandas as pd
+from distutils.util import strtobool
+from sklearn.cluster import KMeans
+
 
 app = Flask(__name__)
+# app.config["DEBUG"] = True
+
 
 @app.route('/', methods=['GET'])
 def home():
 	return "<h1>Nomads Spain app</h1><p>Clustering users api.</p>"
 
+
 @app.route('/cluster', methods=['GET'])
 def api_cluster():
-    user_id = int(request.get_json()['user_id'])
-    surf = int(bool(request.get_json()['Surf']))
+    new_user = pd.DataFrame.from_dict(request.get_json(), orient='index').T
+    user_id = new_user['user_id'].values[0]
+    new_user.drop('user_id', axis='columns', inplace=True)
+    new_user = new_user.applymap(strtobool)
 
-    return jsonify({'user_id': user_id, 'cluster': random.randint(0,5), 'surf': surf})
+    cluster = model.predict(new_user)
+    cluster = int(cluster[0])
+    print(cluster)
+
+    return jsonify({'user_id': user_id, 'cluster': cluster})
 
 if __name__ == '__main__':
+    model = load('model/model.pkl')
     app.run(debug=True)
